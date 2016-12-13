3ac8d9b1fceaf21f0935ec1924a5c9e20e17f6ab
Shir Brass <shirbr510@gmail.com>
2016-12-13 08:40:20 +0200

    added Ilodashnumber methods

--- src/Lodash.Net/Workers/Abstract/ILodashNumber.cs
+++ src/Lodash.Net/Workers/Abstract/ILodashNumber.cs
@@ -2,5 +2,12 @@
 {
     public interface ILodashNumber
     {
+ double Clamp(double number, double lower, double upper);
+
+bool InRange(double number, double start, double upper);
+
+bool InRange(double number, double upper);
+
+double Random(double lower=0,double upper=1, bool floating=false);
     }
 }
