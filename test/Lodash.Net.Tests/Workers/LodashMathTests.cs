9b5c2d0274dc4a8d5ce73b1455e69bd643f34aed
Shir Brass <shirbr510@gmail.com>
2016-12-14 13:49:26 +0200

    Added tests for LodashMath.Add/Substract/Multiply/Divide

--- test/Lodash.Net.Tests/Workers/LodashMathTests.cs
+++ test/Lodash.Net.Tests/Workers/LodashMathTests.cs
@@ -2,13 +2,116 @@
 using Lodash.Net.Tests.Workers.Stubs;
 using Lodash.Net.Workers;
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
 
 namespace Lodash.Net.Tests.Workers
 {
     [TestFixture]
     public class LodashMathTests
     {
+        #region Add
+
+        [Test]
+        [TestCase(2, 3, 5)]
+        [TestCase(-1, 1, 0)]
+        [TestCase(0, 17, 17)]
+        [TestCase(0, -1, -1)]
+        [TestCase(-5, -7, -12)]
+        [TestCase(-5, 7, 2)]
+        [TestCase(-300, 0, -300)]
+        [TestCase(300, 0, 300)]
+        [TestCase(0, 0, 0)]
+        public void Add_Numbers_ReturnsExpectedResult(double num1, double num2, double expectedResult)
+        {
+            //Arrange
+            var lodashMath = new LodashMath(new LodashUtil());
+
+            //Act
+            var result = lodashMath.Add(num1, num2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        #endregion
+
+        #region Substract
+
+        [Test]
+        [TestCase(2, 3, -1)]
+        [TestCase(-1, 1, -2)]
+        [TestCase(0, 17, -17)]
+        [TestCase(0, -1, 1)]
+        [TestCase(-5, -7, 2)]
+        [TestCase(-5, 7, -12)]
+        [TestCase(-300, 0, -300)]
+        [TestCase(300, 0, 300)]
+        [TestCase(0, 0, 0)]
+        public void Substract_Numbers_ReturnsExpectedResult(double num1, double num2, double expectedResult)
+        {
+            //Arrange
+            var lodashMath = new LodashMath(new LodashUtil());
+
+            //Act
+            var result = lodashMath.Substract(num1, num2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        #endregion
+
+        #region Multiply
+
+        [Test]
+        [TestCase(2, 3, 6)]
+        [TestCase(-1, 1, -1)]
+        [TestCase(0, 17, 0)]
+        [TestCase(0, -1, 0)]
+        [TestCase(-5, -7, 35)]
+        [TestCase(-5, 7, -35)]
+        [TestCase(-300, 0, 0)]
+        [TestCase(300, 0, 0)]
+        [TestCase(0, 0, 0)]
+        public void Multiply_Numbers_ReturnsExpectedResult(double num1, double num2, double expectedResult)
+        {
+            //Arrange
+            var lodashMath = new LodashMath(new LodashUtil());
+
+            //Act
+            var result = lodashMath.Multiply(num1, num2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        #endregion
+
+        #region Divide
+
+        [Test]
+        [TestCase(3, 2, 1.5)]
+        [TestCase(-1, 1, -1)]
+        [TestCase(0, 17, 0)]
+        [TestCase(0, -1, 0)]
+        [TestCase(-25, -5, 5)]
+        [TestCase(-25, 5, -5)]
+        [TestCase(-300, 0, double.NegativeInfinity)]
+        [TestCase(300, 0, double.PositiveInfinity)]
+        [TestCase(0, 0, double.NaN)]
+        public void Divide_Numbers_ReturnsExpectedResult(double num1, double num2, double expectedResult)
+        {
+            //Arrange
+            var lodashMath = new LodashMath(new LodashUtil());
+
+            //Act
+            var result = lodashMath.Divide(num1, num2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        #endregion
+
         #region Max
 
         [Test]
