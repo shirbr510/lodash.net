2a54c86a98a4424b9711ec90cc063875aead21b1
Shir Brass <shirbr510@gmail.com>
2016-12-14 17:33:36 +0200

    fix nuget versions of script

--- coveralls.cmd
+++ coveralls.cmd
@@ -1,6 +1,6 @@
-nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory tools
+nuget install NUnit.Runners -Version 3.5.0 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
-nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
+nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
  
 .\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit3-console.exe -targetargs:"/nologo /noshadow .\test\Lodash.Net.Tests\bin\Debug\Lodash.Net.Tests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
  
