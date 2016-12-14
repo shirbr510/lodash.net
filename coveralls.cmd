1ee3c53275398aa7313560c1e6697b4c9b9993d9
Shir Brass <shirbr510@gmail.com>
2016-12-14 17:36:40 +0200

    another nuget versions fix

--- coveralls.cmd
+++ coveralls.cmd
@@ -2,6 +2,6 @@ nuget install NUnit.Runners -Version 3.5.0 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
  
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit3-console.exe -targetargs:"/nologo /noshadow .\test\Lodash.Net.Tests\bin\Debug\Lodash.Net.Tests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.3.5.0\tools\nunit3-console.exe -targetargs:"/nologo /noshadow .\test\Lodash.Net.Tests\bin\Debug\Lodash.Net.Tests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
  
-.\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml 
+.\tools\coveralls.net.0.7\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml 
