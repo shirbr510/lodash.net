1e765889a252d7fee15c2b93607d5c76971bd8ea
Shir Brass <shirbr510@gmail.com>
2016-12-14 17:16:50 +0200

    tiny fix in coveralls script too

--- coveralls.ps1
+++ coveralls.ps1
@@ -2,6 +2,6 @@ nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
  
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe -targetargs:"/nologo /noshadow .\GoogleDriveOfflineBackup.Tests\bin\Debug\GoogleDriveOfflineBackup.Tests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit3-console.exe -targetargs:"/nologo /noshadow .\test\Lodash.Net.Tests\bin\Debug\Lodash.Net.Tests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
  
 .\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml 
